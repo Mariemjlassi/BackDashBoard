69a7cd48f6724d3db378a22c58ae205ffe412b78
Mariem <jlassiimariem52@gmail.com>
2025-05-20 21:23:07 +0100

    first commit

---
+++ src/main/java/com/project/app/service/IDiplomeService.java
@@ -0,0 +1,20 @@
+package com.project.app.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import com.project.app.model.Diplome;
+
+public interface IDiplomeService {
+	
+	public Diplome saveDiplome(Long idType, String libelleTypeDiplome, String libelle);
+	public Diplome saveOrAssignDiplome(Long employeId, String libelle, Long typeDiplomeId, LocalDate dateObtention);
+	public Diplome updateDiplomeEmploye(Long diplomeId, Long employeId, String newLibelle, Long newTypeDiplomeId, LocalDate dateObtention);
+	public void deleteDiplome(Long diplomeId, Long employeId);
+	public List<Diplome> getDiplomesByEmploye(Long employeId);
+	public List<Diplome>getAllDiplomes();
+	public Optional<Diplome> getDiplomeById(Long id);
+	public Diplome updateDiplome(Long id, Long idType, String libelleTypeDiplome, String libelle);
+	public void deleteDiplomePermanently(Long id);
+}
